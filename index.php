7bea9e1336f3b2ee53f3824cef3d35550b6e8890
Anderson <an45lopes@gmail.com>
2025-02-10 20:25:53 -0300

    AULA-02

--- index.php
+++ index.php
@@ -3,11 +3,54 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Portifólio</title>
 
     <link rel="stylesheet" href="css/styles.css" />
 </head>
 <body>
-    
+
+<div class="interface">
+
+ <header class="cabecalho">
+
+  <nav class="menu">
+    <div class="logo">
+     <img src="imagens/logo.png" alt="LOGO" width="150"/>
+    </div>
+    <ul>
+        <li>Home</li>
+        <li>Serviços</li>
+        <li>Produtos</li>
+        <li>Portifolio</li>
+        <li>Sobre</li>
+        <li>Contato</li>
+    </ul>
+
+
+
+    <ol>
+        
+    </ol>
+
+  </nav>
+
+  <div class="titles">
+      <h1 class="titulo">Entregando serviços de qualidade</h1>
+      <h1 class="subtitulo">Soluções em TI</h1>
+
+      <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta deleniti ad veritatis voluptatibus officia vel.</p>
+  </div>
+
+  <div class="socialIcons">
+     <a href="#">Twitter</a>
+     <a href="#">Facebook</a>
+     <a href="#">linkednl</a>
+     <a href="#">Instagram</a>
+  </div>
+
+ </header>
+
+</div>
+
 </body>
 </html>
