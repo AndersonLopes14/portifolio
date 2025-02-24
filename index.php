dc6650a550ec3faadf2e893798a413993aa278c5
Anderson <an45lopes@gmail.com>
2025-02-24 19:27:25 -0300

    Aula 04-24/02/25

--- index.php
+++ index.php
@@ -3,101 +3,53 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>PORTIFOLIO2</title>
-
+    <title>Portifólio</title>
+    
     <link rel="stylesheet" href="css/styles.css" />
-    <style>
-        :root{
-            font-size:16px;
-        }
-        *{
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
 
-        body{
-         /* background-image: url('imagens/logo.png');
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: 100%;
-            background-attachment: fixed;*/
-
-            border:10px solid red;
-            width: 100vw;
-            height:100vw;
-            overflow-x: hidden;
-        }
-
-        p{
-          /*border:1px solid blue;*/
-            border-width: 10px; 
-            border-style: solid;
-            border-color: blue;
-            font-size:3rem;
-        }
-
-        h1{
-            background: yellow;
-            display:block;
-        }
-        
-        h1:hover{
-            background-color:red;
-        }
-
-        .logo{
-            position:fixed;
-            bottom:calc(100vh/2);
-            right:0;
-        }
-
-    </style>
 </head>
 <body>
 
-<div class="interface">
+<div id="interface">
 
- <header class="cabecalho">
+    <header class="cabecalho">
 
-  <nav class="menu">
-    <div class="logo">
-     <img src="imagens/logo.png" alt="LOGO" width="150"/>
-    </div>
-    <ul>
-        <li>Home</li>
-        <li>Serviços</li>
-        <li>Produtos</li>
-        <li>Portifolio</li>
-        <li>Sobre</li>
-        <li>Contato</li>
-    </ul>
+        <nav class="menu">
 
+            <div class="logo">
+                <img src="imagens/logo.png" alt="LOGO" width="150" />
+            </div>
 
+            <ul>
+                <li>Home</li>
+                <li>Serviços</li>
+                <li>Produtos</li>
+                <li>Portifólio</li>
+                <li>Sobre</li>
+                <li>Contato</li>
+            </ul>
 
-    <ol>
-        
-    </ol>
+        </nav>
 
-  </nav>
+        <div class="titles">
 
-  <div class="titles">
-      <h1 class="titulo">Entregando serviços de qualidade</h1>
-      <h1 class="subtitulo">Soluções em TI</h1>
+            <h1 class="titulo">Entregando serviços de qualidade</h1>
+            <h1 class="subtitulo">Soluções em TI</h1>
 
-      <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta deleniti ad veritatis voluptatibus officia vel.</p>
-  </div>
+            <p class="paragrafo">Ave Maria, gratia plena, Dominus tecum: benedicta tu in mulieribus et benedictus fructus ventris tui Iesu. Sancta Maria, Mater Dei, ora pro nobis peccatoribus, nunc et in hora mortis nostrae.Amen.</p>
 
-  <div class="socialIcons">
-     <a href="#">Twitter</a>
-     <a href="#">Facebook</a>
-     <a href="#">linkednl</a>
-     <a href="#">Instagram</a>
-  </div>
+        </div>
 
- </header>
+        <div class="socialIcons">
+            <a href="#">Twitter</a>
+            <a href="#">Facebook</a>
+            <a href="#">LinkedIn</a>
+            <a href="#">Instagram</a>
+        </div>
+
+    </header>
 
 </div>
-
+    
 </body>
 </html>
